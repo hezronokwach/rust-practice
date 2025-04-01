24d644c16ed29e2f79fd8e8c58e543c8d815a0cd
hezronokwach <hezronokwach@gmail.com>
2025-04-01 11:55:35 +0300

    Add looping package with a riddle game in main.rs and update .gitignore

---
+++ looping/src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    let mut trials = 0;
+    let mut ans = String::new();
+    println!(
+        "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+    );
+    loop {
+        ans.clear();
+        io::stdin().read_line(&mut ans).expect("Failed input");
+        trials += 1;
+        let ans2 = ans.trim();
+        if ans2 == "e" {      
+            println!("The letter e");
+            println!("Number of trials {}", trials);
+            break;
+        }
+    }
+}
