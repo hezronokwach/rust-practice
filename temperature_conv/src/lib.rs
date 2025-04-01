f081e2a5f9e2f75ab02a437c93a8ef140ebd575c
hezronokwach <hezronokwach@gmail.com>
2025-04-01 11:24:50 +0300

    Improve fahrenheit to celsius conversion for precision by rounding to 15 decimal places

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f-32.0) * 5.0/9.0
+    ((f - 32.0) * 5.0 / 9.0 * 1000000000000000.0).round() / 1000000000000000.0
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c* 9.0/5.0) + 32.0
+    (c * 9.0 / 5.0) + 32.0
 }
