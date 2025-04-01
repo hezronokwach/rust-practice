33dd74ee1237942c37fab5ed34ac227ad91b6d84
hezronokwach <hezronokwach@gmail.com>
2025-04-01 12:22:54 +0300

    Refactor riddle game main loop to streamline input handling and improve readability

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,26 +1,17 @@
-use std::io;
-
 fn main() {
+    use std::io;
     let mut count = 0;
     let answer = "The letter e";
-
     loop {
         count = count + 1;
-        
+
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
-        
         let mut input = String::new();
-        
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        
-        let input = input.trim().to_lowercase();
-        
-        match input.as_str() {
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let input = input.trim();
+        match input {
             s if s == answer => {
-                println!("The letter e");
-                println!("Number of trials {}", count);
+                println!("Number of trials: {}", count);
                 break;
             }
             _ => {
