8439b546f689130b205aa0279a78e249d4316c8b
hezronokwach <hezronokwach@gmail.com>
2025-04-01 12:07:20 +0300

    Refactor riddle game to store riddle in a variable and streamline input handling

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,24 +1,23 @@
 use std::io;
 
 fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
     let mut trials = 0;
-    let mut ans = String::new();
-    println!(
-        "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
-    );
+    
+    println!("{}", riddle);
+    
     loop {
-        ans.clear();
-        io::stdin().read_line(&mut ans).expect("Failed input");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        
         trials += 1;
-        let ans2 = ans.trim().to_lowercase();
-        if ans2 == "e" || ans2 == "The letter e"{      
+        
+        if input.trim().to_lowercase() == "e" {
             println!("The letter e");
             println!("Number of trials {}", trials);
             break;
-        }else{
-            println!(
-                "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
-            );
+        } else {
+            println!("{}", riddle);
         }
     }
 }
