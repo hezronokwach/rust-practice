1200ed24c701a714d01fbbb6687a2daf1d2c86bd
hezronokwach <hezronokwach@gmail.com>
2025-04-01 12:04:15 +0300

    Enhance riddle game input handling to accept full phrase for letter 'e' and provide additional riddle prompt

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -11,10 +11,14 @@ fn main() {
         io::stdin().read_line(&mut ans).expect("Failed input");
         trials += 1;
         let ans2 = ans.trim().to_lowercase();
-        if ans2 == "e" {      
+        if ans2 == "e" || ans2 == "The letter e"{      
             println!("The letter e");
             println!("Number of trials {}", trials);
             break;
+        }else{
+            println!(
+                "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?"
+            );
         }
     }
 }
