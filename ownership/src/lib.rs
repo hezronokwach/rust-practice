4e8cb1179b3315618c1df2f7822c7c8aff61e15e
hezronokwach <hezronokwach@gmail.com>
2025-04-02 12:20:45 +0300

    Add ownership package with function to extract the first subword from a string

---
+++ ownership/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn first_subword(mut s: String) -> String {
+    for (i, c) in s.chars().enumerate(){
+        if i != 0 && c.is_uppercase() || c == '_' {
+            s.truncate(i);
+            break;
+        }
+    }
+    s    
+}
