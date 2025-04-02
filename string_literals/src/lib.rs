73ee9c642a98bf954f324f772ab1b2c7c5c0f38e
hezronokwach <hezronokwach@gmail.com>
2025-04-02 10:49:37 +0300

    Add string_literals package with utility functions for string manipulation

---
+++ string_literals/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn is_empty(v: &str) -> bool {
+    v.is_empty()
+}
+
+pub fn is_ascii(v: &str) -> bool {
+    v.is_ascii()
+}
+
+pub fn contains(v: &str, pat: &str) -> bool {
+    v.contains(pat)
+}
+
+pub fn split_at(v: &str, index: usize) -> (&str, &str) {
+    v.split_at(index)
+}
+
+pub fn find(v: &str, pat: char) -> usize {
+    let mut j = 0;
+    for (i,c) in v.chars().enumerate(){
+        if c == pat{
+            j = i
+        }
+    }
+    j
+}
