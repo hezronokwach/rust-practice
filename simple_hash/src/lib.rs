70d1d930c0edc6f35e83f60806b3943505b5e403
hokwach <hezronokwach@gmail.com>
2025-04-03 19:51:59 +0300

    Add simple_hash crate with word frequency counter and distinct word count functions

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
+    let mut char_count = HashMap::new();
+    for word in words {
+        let count = char_count.entry(word).or_insert(0);
+        *count += 1;
+    }
+    char_count
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()    
+}
