445ebf1fe80449479fbeb5e474a501667258182e
hezronokwach <hezronokwach@gmail.com>
2025-04-03 13:37:50 +0300

    Refactor Suit and Rank enums for improved random selection and clarity

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,23 +1,30 @@
 use rand::Rng;
+#[derive(Debug, PartialEq)]
 pub enum Suit {
     Heart,
     Diamond,
     Spade,
     Club,
 }
-
+#[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
-    Number(u8),
-    Jack,
-    Queen,
     King,
+    Queen,
+    Jack,
+    Number(u8),
 }
 
 impl Suit {
     pub fn random() -> Suit {
-        let num = rand::rng().random_range(1..=4);
-        Suit::translate(num)
+        let mut random = rand::thread_rng();
+        let i: i32 = random.gen_range(1..5);
+        match i {
+            1 => Suit::Club,
+            2 => Suit::Heart,
+            3 => Suit::Spade,
+            _ => Suit::Diamond,
+        }
     }
 
     pub fn translate(value: u8) -> Suit {
@@ -25,38 +32,50 @@ impl Suit {
             1 => Suit::Heart,
             2 => Suit::Diamond,
             3 => Suit::Spade,
-            4 | _ => Suit::Club,
+            4 => Suit::Club,
+            _ => panic!("invalid suit!!"),
         }
     }
 }
 
 impl Rank {
     pub fn random() -> Rank {
-        let num = rand::rng().random_range(1..=13);
-        Rank::translate(num)
+        let mut random = rand::thread_rng();
+        let i: i32 = random.gen_range(1..14);
+        match i {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            i => Rank::Number(i as u8),
+        }
     }
 
     pub fn translate(value: u8) -> Rank {
         match value {
             1 => Rank::Ace,
-            2..=10 => Rank::Number(value),
             11 => Rank::Jack,
             12 => Rank::Queen,
-            13 | _ => Rank::King,
+            13 => Rank::King,
+            value if value >= 2 && value < 11 => Rank::Number(value),
+            _ => panic!("invalid Rank!!"),
         }
     }
 }
-
+#[derive(Debug, PartialEq)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
 pub fn winner_card(card: &Card) -> bool {
-    if let Suit::Spade = card.suit {
-        if let Rank::Ace = card.rank {
-            return true;
-        }
+    if card
+        == (&Card {
+            suit: Suit::Spade,
+            rank: Rank::Ace,
+        })
+    {
+        return true;
     }
     false
 }
