1d1940a10d28b0e1f3c9fd8c453b8c862544fa5f
hezronokwach <hezronokwach@gmail.com>
2025-04-03 11:46:50 +0300

    Add card_deck package with Card, Suit, and Rank structs, and random selection methods

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,58 @@
+use rand::Rng;
+
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+pub enum Rank {
+    Ace,
+    Number(u8),
+    Jack,
+    Queen,
+    King,
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let num = rand::rng().random_range(1..=4);
+        Suit::translate(num)
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 | _ => Suit::Club,
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let num = rand::rng().random_range(1..=13);
+        Rank::translate(num)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 | _ => Rank::King,
+        }
+    }
+}
+
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    matches!((&card.suit, &card.rank), (&Suit::Spade, &Rank::Ace))
+}
