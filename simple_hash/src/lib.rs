1cb6c4fa47924fa7f235dbe07709763e7b3093dd
hokwach <hezronokwach@gmail.com>
2025-04-03 20:00:17 +0300

    Refactor word_frequency_counter to accept a reference to a vector and simplify nb_distinct_words to return the count of distinct words directly

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,20 +1,14 @@
 use std::collections::HashMap;
 
-pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
+pub fn word_frequency_counter<'a>(words: &'a Vec<&'a str>) -> HashMap<&'a str, usize> {
     let mut char_count = HashMap::new();
     for word in words {
-        let count = char_count.entry(word).or_insert(0);
+        let count = char_count.entry(*word).or_insert(0);
         *count += 1;
     }
     char_count
 }
 
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
-    let mut count: usize = 0;
-    for &value in frequency_count.values() {
-        if value == 1 {
-            count += 1
-        }
-    }
-    count
+    frequency_count.len()
 }
