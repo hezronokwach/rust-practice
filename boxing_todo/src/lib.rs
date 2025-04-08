264f57809a4c49b1359aa4a42e0905b108a0d2bb
hokwach <hezronokwach@gmail.com>
2025-04-08 18:15:44 +0300

    Refactor TodoList and error handling for improved clarity and structure

--- boxing_todo/src/lib.rs
+++ boxing_todo/src/lib.rs
@@ -1,39 +1,70 @@
-use std::{error::Error, fmt::Display};
+mod err;
+use err::{ParseErr, ReadErr};
 
-#[derive(Debug)]
-pub enum ParseErr {
-    Empty,
-    Malformed(Box<dyn Error>),
+pub use json::{parse, stringify};
+pub use std::error::Error;
+use std::{fs::File, io::Read};
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Task {
+    pub id: u32,
+    pub description: String,
+    pub level: u32,
 }
 
-impl Display for ParseErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Failed to parse todo file")
-    }
+#[derive(Debug, Eq, PartialEq)]
+pub struct TodoList {
+    pub title: String,
+    pub tasks: Vec<Task>,
 }
 
-impl Error for ParseErr {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            ParseErr::Empty => None,
-            ParseErr::Malformed(err) => Some(err.as_ref()),
+impl TodoList {
+    pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
+        let mut file = File::open(path).map_err(|e| {
+            Box::new(ReadErr {
+                child_err: Box::new(e),
+            }) as Box<dyn Error>
+        })?;
+        let mut s = String::new();
+        file.read_to_string(&mut s).map_err(|e| {
+            Box::new(ReadErr {
+                child_err: Box::new(e),
+    ///
+    /// # Examples
+    ///
+    /// 
+            }) as Box<dyn Error>
+        })?;
+        if s.trim().is_empty() {
+            return Err(Box::new(ParseErr::Empty));
         }
-    }
-}
+        let parsed_json =
+            parse(&s).map_err(|e| Box::new(ParseErr::Malformed(Box::new(e)))) ?;
+        let title = parsed_json["title"]
+            .as_str()
+            .ok_or_else(|| Box::new(ParseErr::Empty))?
+            .to_string();
 
-#[derive(Debug)]
-pub struct ReadErr {
-    pub child_err: Box<dyn Error>,
-}
+        let mut tasks: Vec<Task> = Vec::new();
+        if parsed_json["tasks"].len() == 0 {
+            return Err(Box::new(ParseErr::Empty));
+        }
 
-impl Display for ReadErr {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Failed to read todo file")
-    }
-}
-
-impl Error for ReadErr {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(self.child_err.as_ref())
+        for i in 0..parsed_json["tasks"].len() {
+            let task = Task {
+                id: parsed_json["tasks"][i]["id"]
+                    .as_u32()
+                    .ok_or(Box::new(ParseErr::Empty))?,
+                description: parsed_json["tasks"][i]["description"]
+                    .as_str()
+                    .ok_or_else(|| Box::new(ParseErr::Empty))?
+                    .to_string(),
+                level: parsed_json["tasks"][i]["level"]
+                    .as_u32()
+                    .ok_or_else(|| Box::new(ParseErr::Empty))?,
+            };
+            tasks.push(task)
+        }
+        Ok(TodoList { title, tasks })
     }
 }
