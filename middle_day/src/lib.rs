ed22e65fcc77395210bd9b47e7ad5b4dc2321bbb
hokwach <hezronokwach@gmail.com>
2025-04-09 12:02:38 +0300

    Refactor middle_day function for improved logic and clarity

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,16 +1,12 @@
-use chrono::{Datelike, NaiveDate, Weekday as wd};
+pub use chrono::{Datelike, NaiveDate, Weekday as wd};
 
 pub fn middle_day(year: i32) -> Option<wd> {
-    let start = NaiveDate::from_ymd_opt(year, 1, 1).unwrap();
-    let end = NaiveDate::from_ymd_opt(year, 12, 31).unwrap();
-
-    let total_days = end.signed_duration_since(start).num_days() + 1;
-
-    if total_days % 2 != 0 {
-        let middle_day_ordinal = (total_days / 2) + 1;
-        let middle_date = start + chrono::Duration::days(middle_day_ordinal as i64 - 1);
-        Some(middle_date.weekday())
-    } else {
-        None
+    let start = NaiveDate::from_ymd_opt(year, 1, 1)?;
+    let end = NaiveDate::from_ymd_opt(year + 1, 1, 1)?;
+    let days = end.signed_duration_since(start).num_days() as u32;
+    if days % 2 == 0 {
+        return None;
     }
+    let z = NaiveDate::from_yo_opt(year, (days / 2) + 1)?;
+    Some(z.weekday())
 }
