e681fac70d9c91192d16aa0f5dc9cc61849bc9f8
hokwach <hezronokwach@gmail.com>
2025-04-12 14:05:45 +0300

    Fix is_pangram function to handle case insensitivity in character checks

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn is_pangram(s: &str) -> bool {
     let mut store : String  = "abcdefghijklmnopqrstuvwxyz".to_string();
-    for c in s.chars(){
-        if store.contains(c){
+    for c in s.chars() {
+        if store.contains(c.to_ascii_lowercase()){
            let index = store.find(c).unwrap();
            store.remove(index);
         }
