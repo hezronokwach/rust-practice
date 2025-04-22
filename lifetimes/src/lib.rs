f30c6bfce94e404077e0aba3c74748409a50ea7d
hezronokwach <hezronokwach@gmail.com>
2025-04-22 16:33:52 +0300

    Refactor lifetimes module to introduce Person struct with name and age fields; remove unused add function and related tests

--- lifetimes/src/lib.rs
+++ lifetimes/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+impl Person<'_> {
+	pub fn new<'a>(name: &'a str) -> Person {
+        Person { name: name, age: 0 }
+	}
 }
