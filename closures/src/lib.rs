b59344903e0244e83f1bff22bc22395e1f23d970
hokwach <hezronokwach@gmail.com>
2025-04-25 11:52:02 +0300

    fix: Square the even numbers

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn first_fifty_even_square() -> Vec<i32> {
     let mut store : Vec<i32> = Vec::new();
-    for i in 2 .. 100{
+    for i in 2 ..=100{
         if i %2 == 0 {
             store.push(i*i);
         }
